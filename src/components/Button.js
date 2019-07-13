39be3e820c4fae38b712ef5e887a8efa7690fb51
Lewis <billobera@gmail.com>
2019-07-13 19:03:10 +0200

    edit color

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,16 +12,16 @@ export default styled.div`
   font-size: 26px;
   font-weight: bold;
   border-radius: 100px;
-  border: 3px solid var(--primary-color);
-  color: var(--primary-color);
+  border: 3px solid var(--action-color);
+  color: var(--action-color);
   transition: box-shadow 342ms cubic-bezier(0.23, 1, 0.32, 1);
 
   :hover {
-    box-shadow: 0px 9px 0 0 var(--primary-color);
+    box-shadow: 0px 9px 0 0 var(--action-color);
   }
 
   :active {
-    box-shadow: 0px 9px 0 0 var(--primary-color);
+    box-shadow: 0px 9px 0 0 var(--action-color);
   }
 
   @media (max-width: 849px) {
