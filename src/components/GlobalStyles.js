45a93f98ed60a41a2370567cd21bd572f97aaa48
Lewis Llobera <billobera@gmail.com>
2019-07-22 18:47:56 +0200

    edit style

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -14,7 +14,6 @@ export default createGlobalStyle`
     margin: 0;
     margin-left: auto;
     margin-right: auto;
-
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
@@ -35,7 +34,6 @@ export default createGlobalStyle`
     margin-inline-start: 0;
     -webkit-margin-end: 0;
     margin-inline-end: 0;
-
     display: inline-block;
     font-size: inherit;
   }
@@ -71,17 +69,16 @@ export default createGlobalStyle`
   }
 
   body {
-    padding-top: 15vh;
+    padding-top: 20vh;
     line-height: 1;
     --sides-padding-desktop: 3%;
     --sides-padding-mobile: 5%;
-
     --light-color: hsl(0, 0%, 99%);
     --light-color-translucent: hsla(0, 0%, 99%, 0.92);
     --dark-color: hsl(0, 0%, 15%);
     --dark-color-light: hsla(0, 0%, 15%, 0.9);
     --dark-color-lighter: hsla(00, 0%, 15%, 0.7);
-    --action-color: hsl(339, 100%, 55%);
+    --accent-color: hsl(339, 100%, 55%);
     background-color: var(--light-color);
     font-family: 'IBMPlexMono', sans-serif;
     font-size: 19px;
