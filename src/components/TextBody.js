1843e8de3585cb8df14d2aa50d6003fd33584baa
Lewis Llobera <billobera@gmail.com>
2019-07-22 18:47:17 +0200

    edit color name

--- src/components/TextBody.js
+++ src/components/TextBody.js
@@ -4,9 +4,9 @@ export default styled.p`
   display: block;
   line-height: 1.5;
   margin: 0 5% 5vh 5%;
-  letter-spacing: -.003em;
+  letter-spacing: -0.003em;
 
-  color: var(--primary-color-light);
+  color: var(--dark-color-light);
 
   @media (max-width: 849px) {
     margin: 0 0 5vh 0;
--- src/components/TextDate.js
+++ src/components/TextDate.js
@@ -4,7 +4,7 @@ import TextBody from './TextBody';
 
 export default styled(TextBody)`
   font-size: 16px;
-  color: var(--primary-color-lighter);
+  color: var(--dark-color-lighter);
 
   @media (max-width: 849px) {
     font-size: 15px;
