be9a2c9642de63865aab4cc418b6c109f07bed3b
Lewis Llobera <billobera@gmail.com>
2019-07-22 18:45:56 +0200

    rename component

--- src/components/index.js
+++ src/components/index.js
@@ -1,28 +1,30 @@
 import Button from './Button';
+import ButtonLink from './ButtonLink';
 import FixedBar from './FixedBar';
 import Footer from './Footer';
 import GlobalStyles from './GlobalStyles';
 import HeaderBack from './HeaderBack';
 import HeaderLogo from './HeaderLogo';
-import HeadingPrimary from './HeadingPrimary';
-import HeadingSecondary from './HeadingSecondary';
+import HeadingL from './HeadingL';
+import HeadingXL from './HeadingXL';
+import Image from './Image';
 import Layout from './Layout';
-import LinkStd from './LinkStd';
 import SEO from './SEO';
 import TextBody from './TextBody';
 import TextDate from './TextDate';
 
 export {
   Button,
+  ButtonLink,
   FixedBar,
   Footer,
   GlobalStyles,
   HeaderBack,
   HeaderLogo,
-  HeadingPrimary,
-  HeadingSecondary,
+  HeadingL,
+  HeadingXL,
+  Image,
   Layout,
-  LinkStd,
   SEO,
   TextBody,
   TextDate,
