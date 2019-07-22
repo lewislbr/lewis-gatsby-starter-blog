76edeecf44c90c391581888eeba2ae5ba10b0feb
Lewis Llobera <billobera@gmail.com>
2019-07-22 18:45:32 +0200

    edit style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,26 @@
 import styled from 'styled-components';
 
-export default styled.div`
+export default styled.a`
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  margin: 0 auto 5vh auto;
+  margin: 10vh auto 5vh auto;
   padding: 20px 50px;
-
-  width: auto;
+  width: 300px;
   font-size: 26px;
   font-weight: bold;
   border-radius: 100px;
-  border: 3px solid var(--action-color);
-  color: var(--action-color);
+  border: 3px solid var(--accent-color);
+  color: var(--accent-color);
   transition: box-shadow 342ms cubic-bezier(0.23, 1, 0.32, 1);
 
   :hover {
-    box-shadow: 0px 9px 0 0 var(--action-color);
+    box-shadow: 0px 9px 0 0 var(--accent-color);
   }
 
   :active {
-    box-shadow: 0px 9px 0 0 var(--action-color);
+    box-shadow: 0px 9px 0 0 var(--accent-color);
   }
 
   @media (max-width: 849px) {
