03030c1d2e08428bee257a128d5794d49efefc4d
Lewis Llobera <billobera@gmail.com>
2019-08-11 22:11:04 +0200

    Refactor styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -8,48 +8,15 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body {
-    height: 100%;
-    width: 100%;
-    margin: 0;
-    margin-left: auto;
-    margin-right: auto;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p {
-    margin: 0;
-    -webkit-margin-before: 0;
-    margin-block-start: 0;
-    -webkit-margin-after: 0;
-    margin-block-end: 0;
-    -webkit-margin-start: 0;
-    margin-inline-start: 0;
-    -webkit-margin-end: 0;
-    margin-inline-end: 0;
-    display: inline-block;
-    font-size: inherit;
-  }
-
-  a {
-    text-decoration: none;
-    -webkit-tap-highlight-color: hsla(0, 0%, 0%, 0);
-    -webkit-tap-highlight-color: transparent;
-    color: inherit;
-  }
-
-  img {
-    max-width: 100%;
-    max-height: 100%;
-    height: 100%;
-    width: 100%;
+  :root {
+    --sides-padding-desktop: 3%;
+    --sides-padding-mobile: 5%;
+    --light-color: hsl(0, 0%, 99%);
+    --light-color-translucent: hsla(0, 0%, 99%, 0.92);
+    --dark-color: hsl(0, 0%, 15%);
+    --dark-color-light: hsla(0, 0%, 15%, 0.9);
+    --dark-color-lighter: hsla(00, 0%, 15%, 0.7);
+    --accent-color: hsl(339, 100%, 55%);
   }
 
   @font-face {
@@ -69,16 +36,15 @@ export default createGlobalStyle`
   }
 
   body {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    margin-left: auto;
+    margin-right: auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     padding-top: 20vh;
     line-height: 1;
-    --sides-padding-desktop: 3%;
-    --sides-padding-mobile: 5%;
-    --light-color: hsl(0, 0%, 99%);
-    --light-color-translucent: hsla(0, 0%, 99%, 0.92);
-    --dark-color: hsl(0, 0%, 15%);
-    --dark-color-light: hsla(0, 0%, 15%, 0.9);
-    --dark-color-lighter: hsla(00, 0%, 15%, 0.7);
-    --accent-color: hsl(339, 100%, 55%);
     background-color: var(--light-color);
     font-family: 'IBMPlexMono', sans-serif;
     font-size: 19px;
@@ -88,5 +54,19 @@ export default createGlobalStyle`
     @media (max-width: 849px) {
       font-size: 17px;
     }
+  }
+
+  a {
+    text-decoration: none;
+    -webkit-tap-highlight-color: hsla(0, 0%, 0%, 0);
+    -webkit-tap-highlight-color: transparent;
+    color: inherit;
+  }
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    height: 100%;
+    width: 100%;
   }
 `;
